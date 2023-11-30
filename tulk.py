55933f94768b58b21463254b8a49e4f7e8fc653b
Ian Harker <ianhharker@protonmail.com>
2023-11-29 20:29:37 -0800

    change regex pattern to include words with ' and - inbetween. Add function to count words in a line

--- tulk.py
+++ tulk.py
@@ -1,6 +1,7 @@
 # Importing the required modules
 from dataclasses import dataclass
 from typing import Union, List, Optional, assert_never
+from collections import defaultdict
 import re
 import argparse
 
@@ -40,7 +41,7 @@ class Word:
 @dataclass
 class Punctuation:
     symbol: str
-    # Is . or ! or ? or ,
+    # If symbol ends a sentence: ! or ? or .
     isHard: bool
 
     def __str__(self):
@@ -63,35 +64,21 @@ class Line:
     utterances: List[Utterance]
 
 
-# Defining the transcript
-transcript = """
-            A: Hello! Hello! Who is this? Nice, it's nice to see you.
-
-            B: And who are you? and -
-
-            F: What? <2.5> What did you say?? Hello??
-
-            ALPH: Back to me!!
-
-            B: Where??
-"""
-
-# Defining the regex pattern for tokenizing the transcript
-# https://regex101.com/r/gs5Y38/1
-pattern = re.compile(
-    r"""
-(?P<speaker>[a-zA-Z]+)(?=:)|
-(?<=<)(?P<pause>\d\.?\d*)(?=>)|
-(?P<word>\w+)(?!:)|
-(?P<puncHard>[.?!,])|
-(?P<puncSoft>['\-\"])
-    """,
-    re.VERBOSE,
-)
-
-
 # Defining the function that returns a linked-list of Subject types
 def parse_transcript(transcript: str) -> Result:
+    # Defining the regex pattern for tokenizing the transcript
+    # https://regex101.com/r/gs5Y38/1
+    pattern = re.compile(
+        r"""
+    (?P<speaker>[a-zA-Z]+)(?=:)|
+    (?<=<)(?P<pause>\d\.?\d*)(?=>)|
+    (?P<word>\w+[-']?\w*)(?!:)|
+    (?P<puncHard>[.?!])|
+    (?P<puncSoft>['\-\",])
+        """,
+        re.VERBOSE,
+    )
+
     # Initialize an empty list that should contain only Subject types
     lineList = []
 
@@ -158,8 +145,11 @@ def subjects_to_str(lines: List) -> str:
         for utterance in line.utterances:
             match utterance:
                 case Word(word):
-                    # If hard punc before word, add a space before word
+                    # If hard punc before word, add a space before this word
                     if isinstance(lastUtter, Punctuation) and lastUtter.isHard:
+                        text += f" {word}"
+                    # If comma before word, add a space before this word
+                    elif isinstance(lastUtter, Punctuation) and lastUtter.symbol == ",":
                         text += f" {word}"
                     # If word before this word, add a space before this word
                     elif isinstance(lastUtter, Word):
@@ -177,6 +167,22 @@ def subjects_to_str(lines: List) -> str:
     return text
 
 
+# Define function to return a dict of wordCounts, given a speaker
+def word_count(lines: Line, targetSpeaker: str) -> dict:
+    wordCount = defaultdict(int)
+    for line in lines:
+        # if guard
+        if line.speaker != targetSpeaker:
+            continue
+        for utterance in line.utterances:
+            match utterance:
+                case Word(word):
+                    # convert word to lower-case and inc word counter
+                    wordCount[(word.lower())] += 1
+    # cast back to dict
+    return dict(wordCount)
+
+
 parser = argparse.ArgumentParser(
     prog="tulk.py",
     description="Interprets transcripted logs between multiple parties!",
@@ -190,9 +196,23 @@ args = parser.parse_args()
 if __name__ == "__main__":
     if args.tests:
         # Test the functions
+        # Defining the transcript
+        transcript = """
+                    A: Hello! Hello! Who is this? Nice, it's nice to see you.
+
+                    B: And who are you? and -
+
+                    F: What? <2.5> What did you say?? Hello??
+
+                    ALPH: Back to me!!
+
+                    B: Where?? Egg-salad and ham!!
+        """
         result = parse_transcript(transcript)
         if isinstance(result, Ok):
             print(subjects_to_str(result.value))
         else:
             print(result)
+        print(word_count(result.value, "A"))
+        print(word_count(result.value, "B"))
         exit()
